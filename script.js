17de16366b449922c841cfe85d732e83c188fed8
Derek Lewis <derek.lewis.den@outlook.com>
2021-07-31 19:05:53 -0600

    fixed the first prompt button

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numberBox = [0,1,2,3,4,5,6,7,8,9];
+var specialBox = "~!@#$%^&*()<>?".split;
+var lowerBox = "abcdefghijklmnopqrstuvwxyz".split;
+var upperBox = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split;
 
-// function () {
-//   var passwordLength = prompt("Enter password length between 8 and 128 characters")
-//   if (passwordLength < 8 || passwordLength > 128)
-//   alert("Please enter a valid password length")
 
-//   return "hello"
-// }
+
+
 
 
 //working code
 function generatePassword () {
-  var passwordLength = prompt("Enter password length between 8 and 128 characters")
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please enter a valid length")
-    
-  }
+  var passwordLength = prompt("Please enter a password length between 8 and 128 characters")
 
-  else {
-    var lowerCase = confirm("Would you like to include lowercase characters in your password?")
-    console.log(lowerCase)
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid length") 
+    generatePassword()
+  
   }
+      var lowerCase = confirm("Would you like to include lowercase characters in your password?")
+      console.log(lowerCase)
       var upperCase = confirm("Would you like to include uppercase characters in your password?");
       console.log(upperCase)
       var specialCharacters = confirm("Would you like to include special characters in your password?");
       console.log(specialCharacters)
       var numbers = confirm("Would you like to include numbers in your password?");
       console.log(numbers)
+    
 
-  if (lowerCase === false && upperCase === false && specialCharacters === false && numbers === false) {
+  if (
+    lowerCase === false && 
+    upperCase === false && 
+    specialCharacters === false && 
+    numbers === false
+    ) {
     alert("Please choose at least one character type")
+    generatePassword()
+
     
 }
 else {
@@ -72,3 +79,5 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //create generatePassword variable - this is where all of the 
+
+//function
